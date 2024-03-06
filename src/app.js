5c6526bd203488cf22c72050ff98e958765a2c35
ronalse <157391307+ronalse@users.noreply.github.com>
2024-03-06 20:36:27 +0000

    ronalse

--- src/app.js
+++ src/app.js
@@ -1,32 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["La", "Nuestra"];
+let adj = ["Mejor", "Gran"];
+let noun = ["Pagina", "Falla"];
+let extension = [".com", ".es", ".ve"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
-
-function dominio() {
-  let pronoun = ["La", "Nuestra"];
-  let adj = ["Mejor", "Gran"];
-  let noun = ["Pagina", "Falla"];
-  let extension = [".com", ".es", ".ve"];
-
-  for (let pronounx in pronoun) {
-    for (let adjx in adj) {
-      for (let nounx in noun) {
-        for (let extensionx in extension) {
-          console.log(
-            pronoun[pronounx] + adj[adjx] + noun[nounx] + extension[extensionx]
-          );
-          return dominio;
-        }
+for (let pronouns in pronoun) {
+  for (let adjs in adj) {
+    for (let nouns in noun) {
+      for (let extensions in extension) {
+        console.log(pronoun[pronouns] + adj[adjs] + noun[nouns] + extension[extensions]);
       }
     }
   }
 }
-domini;
