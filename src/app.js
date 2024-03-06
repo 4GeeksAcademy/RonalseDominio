b1790fbfe94e3e9d4df2d0257261135c73f9ca13
ronalse <157391307+ronalse@users.noreply.github.com>
2024-03-06 20:31:47 +0000

    ronald

--- src/app.js
+++ src/app.js
@@ -9,3 +9,24 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+function dominio() {
+  let pronoun = ["La", "Nuestra"];
+  let adj = ["Mejor", "Gran"];
+  let noun = ["Pagina", "Falla"];
+  let extension = [".com", ".es", ".ve"];
+
+  for (let pronounx in pronoun) {
+    for (let adjx in adj) {
+      for (let nounx in noun) {
+        for (let extensionx in extension) {
+          console.log(
+            pronoun[pronounx] + adj[adjx] + noun[nounx] + extension[extensionx]
+          );
+          return dominio;
+        }
+      }
+    }
+  }
+}
+domini;
