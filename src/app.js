d872aca7ca91a90ceb87d816c3ea69e3fca7eda4
ronalse <157391307+ronalse@users.noreply.github.com>
2024-03-07 17:55:20 +0000

    ronalse

--- src/app.js
+++ src/app.js
@@ -3,12 +3,20 @@ let adj = ["Mejor", "Gran"];
 let noun = ["Pagina", "Falla"];
 let extension = [".com", ".es", ".ve"];
 
-for (let pronouns in pronoun) {
+for (let pronouns in pronoun) { /// en numeros
   for (let adjs in adj) {
     for (let nouns in noun) {
       for (let extensions in extension) {
         console.log(pronoun[pronouns] + adj[adjs] + noun[nouns] + extension[extensions]);
       }
     }
+  }
+}
+for (let i = 0 ; i < pronoun.length; i++) {
+  for(let j = 0; j < adj.length; j++) {
+    for(let k = 0; k < noun.length; k++)
+      for (let l = 0; l < extension.length; l++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + extension[l])
+      }
   }
 }
